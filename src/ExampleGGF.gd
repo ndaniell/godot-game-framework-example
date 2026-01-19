ecdb7b1b8cd32ebcfd3d69ed56ef38945f799919
Nicholas Daniell <nick.daniell@gmail.com>
2026-01-19 15:46:39 -0500

    ci: fix gdlint config and line lengths

--- src/ExampleGGF.gd
+++ src/ExampleGGF.gd
@@ -1,6 +1,9 @@
 extends "res://addons/godot_game_framework/addons/godot_game_framework/GGF.gd"
 
 const MANAGER_NODE_PREFIX := "GGF_"
+const _MANAGERS_BASE := (
+	"res://addons/godot_game_framework/" + "addons/godot_game_framework/core/managers/"
+)
 
 
 func _bootstrap() -> void:
@@ -14,75 +17,51 @@ func _bootstrap() -> void:
 
 	_ensure_manager(
 		&"LogManager",
-		_load_script(
-			"res://addons/godot_game_framework/addons/godot_game_framework/core/managers/LogManager.gd"
-		)
+		_load_script(_MANAGERS_BASE + "LogManager.gd")
 	)
 	_ensure_manager(
 		&"EventManager",
-		_load_script(
-			"res://addons/godot_game_framework/addons/godot_game_framework/core/managers/EventManager.gd"
-		)
+		_load_script(_MANAGERS_BASE + "EventManager.gd")
 	)
 	_ensure_manager(
 		&"NotificationManager",
-		_load_script(
-			"res://addons/godot_game_framework/addons/godot_game_framework/core/managers/NotificationManager.gd"
-		)
+		_load_script(_MANAGERS_BASE + "NotificationManager.gd")
 	)
 	_ensure_manager(
 		&"SettingsManager",
-		_load_script(
-			"res://addons/godot_game_framework/addons/godot_game_framework/core/managers/SettingsManager.gd"
-		)
+		_load_script(_MANAGERS_BASE + "SettingsManager.gd")
 	)
 	_ensure_manager(
 		&"AudioManager",
-		_load_script(
-			"res://addons/godot_game_framework/addons/godot_game_framework/core/managers/AudioManager.gd"
-		)
+		_load_script(_MANAGERS_BASE + "AudioManager.gd")
 	)
 	_ensure_manager(
 		&"TimeManager",
-		_load_script(
-			"res://addons/godot_game_framework/addons/godot_game_framework/core/managers/TimeManager.gd"
-		)
+		_load_script(_MANAGERS_BASE + "TimeManager.gd")
 	)
 	_ensure_manager(
 		&"ResourceManager",
-		_load_script(
-			"res://addons/godot_game_framework/addons/godot_game_framework/core/managers/ResourceManager.gd"
-		)
+		_load_script(_MANAGERS_BASE + "ResourceManager.gd")
 	)
 	_ensure_manager(
 		&"PoolManager",
-		_load_script(
-			"res://addons/godot_game_framework/addons/godot_game_framework/core/managers/PoolManager.gd"
-		)
+		_load_script(_MANAGERS_BASE + "PoolManager.gd")
 	)
 	_ensure_manager(
 		&"SceneManager",
-		_load_script(
-			"res://addons/godot_game_framework/addons/godot_game_framework/core/managers/SceneManager.gd"
-		)
+		_load_script(_MANAGERS_BASE + "SceneManager.gd")
 	)
 	_ensure_manager(
 		&"SaveManager",
-		_load_script(
-			"res://addons/godot_game_framework/addons/godot_game_framework/core/managers/SaveManager.gd"
-		)
+		_load_script(_MANAGERS_BASE + "SaveManager.gd")
 	)
 	_ensure_manager(
 		&"NetworkManager",
-		_load_script(
-			"res://addons/godot_game_framework/addons/godot_game_framework/core/managers/NetworkManager.gd"
-		)
+		_load_script(_MANAGERS_BASE + "NetworkManager.gd")
 	)
 	_ensure_manager(
 		&"InputManager",
-		_load_script(
-			"res://addons/godot_game_framework/addons/godot_game_framework/core/managers/InputManager.gd"
-		)
+		_load_script(_MANAGERS_BASE + "InputManager.gd")
 	)
 
 	# Project-owned game flow.
@@ -90,7 +69,5 @@ func _bootstrap() -> void:
 
 	_ensure_manager(
 		&"UIManager",
-		_load_script(
-			"res://addons/godot_game_framework/addons/godot_game_framework/core/managers/UIManager.gd"
-		)
+		_load_script(_MANAGERS_BASE + "UIManager.gd")
 	)
